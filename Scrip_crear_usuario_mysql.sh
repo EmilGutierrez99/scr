95be5fef29bf9f410f48e00f20bd36d877b802ea
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-10-10 09:38:34 -0400

    cambio 3

--- Scrip_crear_usuario_mysql.sh
+++ Scrip_crear_usuario_mysql.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Variables
-DB_NAME="DB_002"
-DB_USER="USER_002"
-DB_PASS="PASSWORD_002"
+DB_NAME="DB_003"
+DB_USER="USER_003"
+DB_PASS="PASSWORD_003"
 ROOT_USER="root"
 ROOT_PASS="password"
+TABLE_NAME="TABLE_002"
 
 
 # Crear base de datos
@@ -17,11 +18,11 @@ mysql -u $ROOT_USER -p"$ROOT_PASS" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$D
 mysql -u $ROOT_USER -p"$ROOT_PASS" -e "FLUSH PRIVILEGES;"
 
 # Verificar si el usuario puede crear una tabla
-mysql -u $DB_USER -p"$DB_PASS" -e "USE $DB_NAME; CREATE TABLE IF NOT EXISTS TABLE_001 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50));"
+mysql -u $DB_USER -p"$DB_PASS" -e "USE $DB_NAME; CREATE TABLE IF NOT EXISTS $TABLE_NAME (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50));"
 
 # Comprobación final
 if [ $? -eq 0 ]; then
-  echo "El usuario $DB_USER ha creado la tabla TABLE_001 correctamente en la base de datos $DB_NAME."
+  echo "El usuario $DB_USER ha creado la tabla $TABLE_NAME correctamente en la base de datos $DB_NAME."
 else
   echo "Hubo un error al intentar crear la tabla con el usuario $DB_USER."
 fi
@@ -29,6 +30,41 @@ fi
 echo "Listado de usuarios MySQL:"
 mysql -u "$ROOT_USER" -p"$ROOT_PASS" -e "SELECT user, host FROM mysql.user;"
 
+echo "Listado de bases de datos en el servidor MySQL:"
+mysql -u "$ROOT_USER" -p"$ROOT_PASS" -e "SHOW DATABASES;"
+
+#####
+# Mostrar quién tiene acceso a cada base de datos
+echo "Usuarios con acceso a las bases de datos:"
+mysql -u "$ROOT_USER" -p"$ROOT_PASS" -e "
+SELECT
+    db.Db AS DatabaseName,
+    user.User AS UserName,
+    user.Host AS UserHost,
+    db.Select_priv AS SelectPrivilege,
+    db.Insert_priv AS InsertPrivilege,
+    db.Update_priv AS UpdatePrivilege,
+    db.Delete_priv AS DeletePrivilege,
+    db.Create_priv AS CreatePrivilege,
+    db.Drop_priv AS DropPrivilege,
+    db.Grant_priv AS GrantPrivilege,
+    db.References_priv AS ReferencesPrivilege
+FROM
+    mysql.db AS db
+INNER JOIN
+    mysql.user AS user
+ON
+    db.User = user.User;
+"
+
+# Comprobación final
+if [ $? -eq 0 ]; then
+  echo "Listado de bases de datos y permisos mostrado correctamente."
+else
+  echo "Hubo un error al mostrar las bases de datos o permisos."
+fi
+
+######
 # Mostrar todas las tablas en la base de datos especificada
 echo "Listado de tablas en la base de datos $DB_NAME:"
 mysql -u "$ROOT_USER" -p"$ROOT_PASS" -e "SHOW TABLES FROM $DB_NAME;"
