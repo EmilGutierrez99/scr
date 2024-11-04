a26670d37e3433113c060338cede0ff8981d328d
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-11-04 10:02:27 -0400

    update 1.0

--- wp_setup_db.sh
+++ wp_setup_db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Verificar que se han pasado los tres argumentos necesarios
 if [ "$#" -ne 3 ]; then
     echo "Uso: ./wp_setup_db.sh <nombre_db> <usuario_db> <contraseña_db>"
@@ -8,6 +10,10 @@ fi
 DB_NAME=$1
 DB_USER=$2
 DB_PASSWORD=$3
+
+#Usuario root
+ROOT_USER="root"
+ROOT_PASS="password"
 
 # Mensaje de confirmación de inicio
 echo "Iniciando configuración de la base de datos de WordPress..."
@@ -21,9 +27,9 @@ mysql -u root -p -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_ge
 
 # Crear el usuario y asignarle permisos
 echo "Creando usuario y asignando permisos..."
-mysql -u root -p -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
-mysql -u root -p -e "FLUSH PRIVILEGES;"
+mysql -u $ROOT_USER -p $ROOT_PASS -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u $ROOT_USER -p $ROOT_PASS -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+mysql -u $ROOT_USER -p $ROOT_PASS -e "FLUSH PRIVILEGES;"
 
 # Confirmación de finalización
 echo "La configuración de la base de datos ha finalizado exitosamente."
