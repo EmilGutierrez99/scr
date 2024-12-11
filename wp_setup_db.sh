c238a65a113b95603e401fde97dd679f1d75dcaf
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-12-11 19:44:39 -0400

    8.8

--- wp_setup_db.sh
+++ wp_setup_db.sh
@@ -158,31 +158,23 @@ verificar_user_exists_des() {
 if [ "$#" -ne 4 ]; then
     echo "Uso: ./wp_setup_db.sh <nombre_db> <usuario_db> <contraseña_db> <tabla_db>"
     echo "No se proporcionaron los argumentos necesarios. Por favor, ingréselos manualmente."
-    while true; do
         read -p "Ingresa el nombre de la DB (8-64 caracteres, a-z, A-Z, 0-9, _): " DB_NAME
-        DB_NAME=$(validar_longitud_regex "$DB_NAME")
-        DB_NAME=$(validar_caracteres_regex "$DB_NAME")
-        DB_NAME=$(verificar_database_exists "$DB_NAME")
-        [ -n "$DB_NAME" ] && break
-    done
-    while true; do
+        validar_longitud_regex "$DB_NAME"
+        validar_caracteres_regex "$DB_NAME"
+        verificar_database_exists "$DB_NAME"
+    
         read -p "Ingresa el nombre del nuevo USER (8-64 caracteres, a-z, A-Z, 0-9, _): " DB_USER
-        DB_USER=$(validar_longitud_regex "$DB_USER")
-        DB_USER=$(validar_caracteres_regex "$DB_USER")
-        DB_USER=$(verificar_user_exists "$DB_USER")
-        [ -n "$DB_USER" ] && break
-    done
-    while true; do
+        validar_longitud_regex "$DB_USER"
+        validar_caracteres_regex "$DB_USER"
+        verificar_user_exists "$DB_USER"
+      
         read -p "Ingresa el Password del nuevo USER (8-64 caracteres): " USER_PASS
-        USER_PASS=$(validar_longitud_regex "$USER_PASS")
-        [ -n "$USER_PASS" ] && break
-    done
-    while true; do
+        validar_longitud_regex "$USER_PASS"
+  
         read -p "Ingresa el nombre de la nueva Tabla (8-64 caracteres, a-z, A-Z, 0-9, _): " TABLE_NAME
-        TABLE_NAME=$(validar_longitud_regex "$TABLE_NAME")
-        TABLE_NAME=$(validar_caracteres_regex "$TABLE_NAME")
-        [ -n "$TABLE_NAME" ] && break
-    done        
+        validar_longitud_regex "$TABLE_NAME"
+        validar_caracteres_regex "$TABLE_NAME"
+          
 else 
     # VARIABLES
     DB_NAME=$1
