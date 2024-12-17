2096b2b83854eec7ed37421caaf72a1f6b1e7441
EmilGutierrez99 <emilgutierrez81@gmail.com>
2024-12-16 22:20:05 -0400

    9.7

---
+++ amp_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR_USER="/home/user/scr"
+
+cd $DIR_USER/
+sudo chmod +x mysql_installer.sh
+./mysql_installer.sh
+
+cd $DIR_USER/
+sudo chmod +x apache_installer.sh
+./apache_installer.sh
+
+cd $DIR_USER/
+sudo chmod +x php_installer.sh
+./php_installer.sh
+
+
